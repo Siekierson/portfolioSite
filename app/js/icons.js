ca05a5025ea3bd7a4a9ac1f41ea8c8f0a9f16575
Siekierson <adrian.siekierko@gmail.com>
2021-04-27 13:41:32 +0200

    icons done

--- app/js/icons.js
+++ app/js/icons.js
@@ -1,12 +1,16 @@
-import { frontIcons, backIcons, toolsIcons } from "./iconsConfig";
-const categories = document.querySelectorAll(".category>div");
-const front = frontIcons.map(
-  (item) => `
-<div class="iconContainer"><img src="${item.photoSrc}" alt="technologyLogo"/></div>
-`
-);
+import { frontIcons, backIcons, toolsIcons } from "./iconsConfig.js";
 const addIcons = () => {
-  //   categories.innerHTML = front;
-  console.log(front);
+  const data = [frontIcons, backIcons, toolsIcons];
+  const categories = document.querySelectorAll(".category>div");
+  const dataToString = data.map((el) =>
+    el
+      .map(
+        (item) =>
+          `<div class="iconContainer" onMouseOver="this.style.backgroundColor='${item.color}'"
+          onMouseOut="this.style.backgroundColor='transparent'"><img src="${item.photoSrc}" alt="technologyLogo" /><h3>${item.name}</h3></div>`
+      )
+      .join("")
+  );
+  categories.forEach((item, index) => (item.innerHTML = dataToString[index]));
 };
 export default addIcons;
--- app/js/iconsConfig.js
+++ app/js/iconsConfig.js
@@ -1,76 +1,77 @@
 export const frontIcons = [
   {
     name: "HTML",
-    photoSrc: "//assets/icons/html.svg",
+    photoSrc: ".//assets/icons/html.svg",
     color: "#E34F26",
   },
   {
     name: "CSS",
-    photoSrc: "//assets/icons/css.svg",
+    photoSrc: ".//assets/icons/css.svg",
     color: "#1572B6",
+  },
+
+  {
+    name: "SCSS",
+    photoSrc: ".//assets/icons/scss.svg",
+    color: "#CC6699",
   },
   {
     name: "JavaScript",
-    photoSrc: "//assets/icons/js.svg",
+    photoSrc: ".//assets/icons/js.svg",
     color: "#F7DF1E",
   },
   {
     name: "TypeScript",
-    photoSrc: "//assets/icons/typescript.svg",
+    photoSrc: ".//assets/icons/typescript.svg",
     color: "#3178C6",
   },
   {
     name: "React.js",
-    photoSrc: "//assets/icons/react.svg",
+    photoSrc: ".//assets/icons/react.svg",
     color: "#61DAFB",
   },
   {
     name: "styled-components",
-    photoSrc: "//assets/icons/styled-components.svg",
+    photoSrc: ".//assets/icons/styled-components.svg",
     color: "#DB7093",
-  },
-  {
-    name: "SCSS",
-    photoSrc: "//assets/icons/scss.svg",
-    color: "#CC6699",
   },
 ];
 export const backIcons = [
   {
     name: "Node.js",
-    photoSrc: "//assets/icons/node.svg",
+    photoSrc: ".//assets/icons/node.svg",
     color: "#339933",
   },
   {
     name: "Express.js",
-    photoSrc: "//assets/icons/express.svg",
+    photoSrc: ".//assets/icons/express.svg",
     color: "#000000",
   },
   {
     name: "MongoDB",
-    photoSrc: "//assets/icons/mongodb.svg",
+    photoSrc: ".//assets/icons/mongodb.svg",
     color: "#47A248",
   },
 ];
 export const toolsIcons = [
   {
     name: "Git",
-    photoSrc: "//assets/icons/git.svg",
+    photoSrc: ".//assets/icons/git.svg",
     color: "#F05032",
   },
   {
     name: "Github",
-    photoSrc: "//assets/icons/github.svg",
+    photoSrc: ".//assets/icons/github.svg",
     color: "#181717",
   },
   {
     name: "Figma",
-    photoSrc: "//assets/icons/figma.svg",
+    photoSrc: ".//assets/icons/figma.svg",
     color: "#F24E1E",
   },
   {
     name: "Postman",
-    photoSrc: "//assets/icons/postman.svg",
+    photoSrc: ".//assets/icons/postman.svg",
     color: "#FF6C37",
   },
 ];
